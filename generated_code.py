80a5421682d4e08e789597fbfcf476fe6480a93a
Nikolaos Konstantinou <77862574+nccon@users.noreply.github.com>
2023-06-20 21:30:55 +0300

    Initial commit

---
+++ generated_code.py
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Project Management</title>
+    <style>
+      body {
+        background-image: linear-gradient(to right, #00a8ff, #ffffff);
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Welcome to my Project v2</h1>
+    <p>This project has been build using AI.</p>
